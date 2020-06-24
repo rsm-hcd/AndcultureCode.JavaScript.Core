c902bb8a459e0838ba33554bda6d2b4745dc0a1a
Winton DeShong <contact@winton.me>
2020-06-24 16:22:21 -0400

    First pass implementation abstraction of i18n related configuration utilities

---
+++ src/cultures/spanish-spain-base.ts
@@ -0,0 +1,9 @@
+import { Rfc4646LanguageCodes } from "../constants/rfc4646-language-codes";
+import { Culture } from "../interfaces/culture";
+
+const SpanishSpainBase: Culture<any> = {
+    code: Rfc4646LanguageCodes.ES_ES,
+    resources: null,
+};
+
+export default SpanishSpainBase;
--- src/utilities/service-utils.ts
+++ src/utilities/service-utils.ts
@@ -1,12 +1,89 @@
-import { AxiosResponse } from "axios";
+import { LocalizationUtils } from "./localization-utils";
+import { StringUtils } from "./string-utils";
+import axios, { AxiosResponse } from "axios";
 import { ServiceResponse } from "../interfaces/service-response";
 import { Result } from "../interfaces/result";
 import { ResultRecord } from "../view-models/result-record";
 import { PagedResult } from "../interfaces/paged-result";
+import { HttpHeader } from "../enumerations/http-header";
+import { ContentType } from "../enumerations/content-type";
+
+// -----------------------------------------------------------------------------------------
+// #region Variables
+// -----------------------------------------------------------------------------------------
+
+let _currentCultureCode: string;
+const _defaultApiUrl = "/api/v1";
+
+//#endregion Variables
 
 // -----------------------------------------------------------------------------------------
 // #region Functions
 // -----------------------------------------------------------------------------------------
+
+/**
+ * Configures global service properties for your application
+ * @param cultureCode Current culture code of the application
+ * @param onApiResponseError Global handler called whenever there is an erroneous API response
+ * @param onApiResponseSuccess Global handler called whenever there is a successful API response
+ */
+const _configure = (
+    cultureCode?: string,
+    onApiResponseError?: (error: any) => Promise<unknown>,
+    onApiResponseSuccess?: (response: AxiosResponse) => AxiosResponse<any>
+) => {
+    _configureCultureCode(cultureCode);
+    _configureHeaders();
+    _configureInterceptors(onApiResponseError, onApiResponseSuccess);
+};
+
+/**
+ * Configure application's services for the provided culture
+ * @param cultureCode Requested RFC-4646 language code
+ * @param apiUrl Application's base api url (hopefully versioned)
+ */
+const _configureCultureCode = (
+    cultureCode?: string,
+    apiUrl: string = _defaultApiUrl
+) => {
+    if (StringUtils.isEmpty(cultureCode)) {
+        cultureCode = LocalizationUtils.defaultCultureCode();
+    }
+
+    if (_currentCultureCode === cultureCode) {
+        return;
+    }
+
+    _currentCultureCode = cultureCode!;
+
+    axios.defaults.baseURL = `/${_currentCultureCode.toLowerCase()}${apiUrl}`;
+};
+
+/**
+ * Configure axios HTTP headers
+ */
+const _configureHeaders = () => {
+    axios.defaults.headers.post[HttpHeader.Accept] = ContentType.Json;
+    axios.defaults.headers.post[HttpHeader.ContentType] = ContentType.Json;
+    axios.defaults.headers.put[HttpHeader.Accept] = ContentType.Json;
+    axios.defaults.headers.put[HttpHeader.ContentType] = ContentType.Json;
+};
+
+/**
+ * Configures axios global API interceptors
+ * @param onApiResponseError
+ * @param onApiResponseSuccess
+ */
+const _configureInterceptors = (
+    onApiResponseError?: (error: any) => Promise<unknown>,
+    onApiResponseSuccess?: (response: AxiosResponse) => AxiosResponse<any>
+) => {
+    if (onApiResponseError == null && onApiResponseSuccess == null) {
+        return;
+    }
+
+    axios.interceptors.response.use(onApiResponseSuccess, onApiResponseError);
+};
 
 /**
  * Translates axios specific data response to a more generic ServiceResponse
@@ -70,6 +147,8 @@ const _mapPagedAxiosResponse = <TRecord>(
 // -----------------------------------------------------------------------------------------
 
 export const ServiceUtils = {
+    configure: _configure,
+    configureCultureCode: _configureCultureCode,
     mapAxiosResponse: _mapAxiosResponse,
     mapPagedAxiosResponse: _mapPagedAxiosResponse,
 };
