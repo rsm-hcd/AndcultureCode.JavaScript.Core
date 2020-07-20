ed224cc1db95cddd1db9d7c38c6ad0b45ddbf465
Winton DeShong <contact@winton.me>
2020-07-20 13:09:42 -0400

    Moved BrowserUtils test cleanup into 'afterEach'

--- src/utilities/browser-utils.test.ts
+++ src/utilities/browser-utils.test.ts
@@ -1,54 +1,56 @@
 import { BrowserUtils } from "./browser-utils";
 
 describe("BrowserUtils", () => {
-  describe("isIE", () => {
-    it("when wondow.document.documentMode is undefined, then returns false", () => {
-      // Arrange
-      window.document.documentMode = undefined;
-
-      // Act
-      const result = BrowserUtils.isIE();
-
-      // Assert
-      expect(result).toBeFalse();
+    afterEach(() => {
+        // Clean up
+        window.document.documentMode = undefined;
     });
 
-    it("when window.document.documentMode is truthy, then returns true", () => {
-      // Arrange
-      window.document.documentMode = true;
+    describe("isIE", () => {
+        it("when wondow.document.documentMode is undefined, then returns false", () => {
+            // Arrange
+            window.document.documentMode = undefined;
 
-      // Act
-      const result = BrowserUtils.isIE();
+            // Act
+            const result = BrowserUtils.isIE();
 
-      // Assert
-      expect(result).toBeTrue();
+            // Assert
+            expect(result).toBeFalse();
+        });
+
+        it("when window.document.documentMode is truthy, then returns true", () => {
+            // Arrange
+            window.document.documentMode = true;
+
+            // Act
+            const result = BrowserUtils.isIE();
+
+            // Assert
+            expect(result).toBeTrue();
+        });
     });
-  });
 
-  describe("isNotIE", () => {
-    it("when wondow.document.documentMode is undefined, then returns true", () => {
-      // Arrange
-      window.document.documentMode = undefined;
+    describe("isNotIE", () => {
+        it("when wondow.document.documentMode is undefined, then returns true", () => {
+            // Arrange
+            window.document.documentMode = undefined;
 
-      // Act
-      const result = BrowserUtils.isNotIE();
+            // Act
+            const result = BrowserUtils.isNotIE();
 
-      // Assert
-      expect(result).toBeTrue();
+            // Assert
+            expect(result).toBeTrue();
+        });
+
+        it("when window.document.documentMode is truthy, then returns false", () => {
+            // Arrange
+            window.document.documentMode = true;
+
+            // Act
+            const result = BrowserUtils.isNotIE();
+
+            // Assert
+            expect(result).toBeFalse();
+        });
     });
-
-    it("when window.document.documentMode is truthy, then returns false", () => {
-      // Arrange
-      window.document.documentMode = true;
-
-      // Act
-      const result = BrowserUtils.isNotIE();
-
-      // Assert
-      expect(result).toBeFalse();
-    });
-  });
-
-  // Clean up
-  window.document.documentMode = undefined;
 });
