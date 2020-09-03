b57023179047e7fe7329438296cb1d981586a109
Said Shah <sshah@andculture.com>
2020-09-03 14:49:35 -0400

    Added tests for enumToArray and getRandomenum

--- src/utilities/core-utils.test.ts
+++ src/utilities/core-utils.test.ts
@@ -1,13 +1,88 @@
 import { CoreUtils } from "./core-utils";
 
 describe("CoreUtils", () => {
+    // -----------------------------------------------------------------------------------------
+    // #region enumToArray
+    // -----------------------------------------------------------------------------------------
+
     describe("enumToArray", () => {
-        test.skip("TODO: Backfill tests to complete issue https://github.com/AndcultureCode/AndcultureCode.JavaScript.Core/issues/29", () => {});
+        test("when given enum, returns array of values", () => {
+            // Arrange
+            enum testEnum {
+                first = "First",
+                second = "Second",
+            }
+
+            // Act
+            const result = CoreUtils.enumToArray(testEnum);
+
+            // Assert
+            expect(result).toContain(testEnum.first);
+            expect(result).toContain(testEnum.second);
+        });
+
+        test("when given null enum, returns empty array", () => {
+            // Act
+            const result = CoreUtils.enumToArray(null);
+
+            // Assert
+            expect(result).toHaveLength(0);
+            expect(result).toBeInstanceOf(Array);
+        });
     });
 
+    // #endregion enumToarray
+
+    // -----------------------------------------------------------------------------------------
+    // #region getRandomEnum
+    // -----------------------------------------------------------------------------------------
+
     describe("getRandomEnum", () => {
-        test.skip("TODO: Backfill tests to complete issue https://github.com/AndcultureCode/AndcultureCode.JavaScript.Core/issues/29", () => {});
+        test("when given enum, returns random value", () => {
+            // Arrange
+            enum testEnum {
+                first = "First",
+                second = "Second",
+                third = "Third",
+            }
+            const expectedValues = [
+                testEnum.first,
+                testEnum.second,
+                testEnum.third,
+            ];
+
+            // Act
+            const result = CoreUtils.getRandomEnum(testEnum);
+
+            // Assert
+            expect(result).not.toBeNull();
+            expect(expectedValues).toContain(result);
+        });
+
+        test("when given enum and exclude element, returns random value and not excluded element", () => {
+            // Arrange
+            enum testEnum {
+                first = "First",
+                second = "Second",
+                third = "Third",
+            }
+            const expectedValues = [testEnum.first, testEnum.second];
+
+            // Act
+            const result = CoreUtils.getRandomEnum(testEnum, testEnum.third);
+
+            // Assert
+            expect(result).not.toBeNull();
+            expect(expectedValues).toContain(result);
+            expect(result).not.toBe(testEnum.third);
+        });
     });
+
+    // #endregion getRandomEnum
+
+    // -----------------------------------------------------------------------------------------
+    // #region timer
+    // -----------------------------------------------------------------------------------------
 
     describe("timer", () => {
         test("returns timer object", () => {
@@ -28,6 +103,10 @@ describe("CoreUtils", () => {
             expect(result.stop).not.toBeUndefined();
             expect(result.stop).toBeInstanceOf(Function);
         });
+
+        // -----------------------------------------------------------------------------------------
+        // #region timer#stop
+        // -----------------------------------------------------------------------------------------
 
         describe("timer#stop", () => {
             test("returns elapsed time", async () => {
@@ -93,6 +172,10 @@ describe("CoreUtils", () => {
                 // Assert
                 expect(consoleSpy.mock.calls[0]).toContain(result);
             });
-        }); // end timer#stop
-    }); // end timer
+        });
+
+        // #endregion timer#stop
+    });
+
+    // #endregion timer
 });
