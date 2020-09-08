fe7665bca64f262b0f205ca827914bfe32e53d7d
Said Shah <sshah@andculture.com>
2020-09-08 10:27:37 -0400

    Added Unit of time enum

---
+++ src/enumerations/unit-of-time.ts
@@ -0,0 +1,20 @@
+enum UnitOfTime {
+    Day = "day",
+    Days = "days",
+    Hour = "hour",
+    Hours = "hours",
+    Millisecond = "millisecond",
+    Milliseconds = "milliseconds",
+    Minute = "minute",
+    Minutes = "minutes",
+    Month = "month",
+    Months = "months",
+    Second = "second",
+    Seconds = "seconds",
+    Week = "week",
+    Weeks = "weeks",
+    Year = "year",
+    Years = "years",
+}
+
+export { UnitOfTime };
