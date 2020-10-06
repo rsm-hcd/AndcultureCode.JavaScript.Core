81c56241deb9a3a66130e8859cadb59d5409a33b
Kevin Busch <kbusch@andculture.com>
2020-10-06 09:32:25 -0400

    Removed global index file and replaced with regular index file

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,17 @@
-// const path = require('path');
-
-// export default () => (
-//     {
-//         mode: 'production',
-//         entry: './index.js',
-//         output: {
-//             path: path.resolve(__dirname, './dist'),
-//             filename: 'webpack-numbers.js',
-//             libraryTarget: 'umd',
-//             globalObject: 'this',
-//             library: 'AndcultureCode' // <---- Casing? `AndcultureCode` or `andcultureCode`?
-//         },
-//         externals: {
-//             'lodash': {
-//                 commonjs: 'lodash',
-//                 commonjs2: 'lodash',
-//                 amd: 'lodash',
-//                 root: '_'
-//             }
-//         },
-//         module: {
-//             rules: [
-//                 {
-//                     test: /\.(js)$/,
-//                     exclude: /(node_modules|bower_components)/,
-//                     use: 'babel-loader'
-//                 }
-//             ]
-//         },
-//     }
-// );
-
 const path = require('path');
 
 module.exports = {
-    entry: './src/index-global.ts',
-    externals: { // <---- externals defines those peer dependencies that should not be bundled in global output file and instead loaded by the consumer prior to loading this global output file
+    entry: './src/index.ts',
+
+    /*
+        Externals below defines those peer dependencies that should not be bundled in global output
+        file and instead be loaded by the consumer prior to loading the distributable that webpack
+        generates.
+
+        NOTE: Any externals that are commented out below are being included in the webpack
+            distriburatable
+    */
+    externals: {
         "axios": {
             commonjs: 'axios',
             commonjs2: 'axios',
@@ -66,7 +42,7 @@ module.exports = {
             amd: 'lodash',
             root: '_',
         },
-        // queryString: { <---- Vendoring (including in output) this library since it uses a module loader and it's a fairly small source lift
+        // queryString: {
         //     commonjs: 'query-string',
         //     commonjs2: 'query-string',
         //     amd: 'query-string',
