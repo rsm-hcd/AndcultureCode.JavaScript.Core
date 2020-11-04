970c6b7e5f69809785c58d89fb56d3b842c44854
Nhinh Dao <ndao@andculture.com>
2020-11-04 17:01:45 -0500

    Port lodash methods to private functions

--- src/utilities/core-utils.ts
+++ src/utilities/core-utils.ts
@@ -1,9 +1,60 @@
 import { CollectionUtils } from "./collection-utils";
 import _ from "lodash";
+import {
+    Cancelable,
+    CurriedFunction1,
+    MemoizedFunction,
+    ThrottleSettings
+} from "lodash";
 
 // -----------------------------------------------------------------------------------------
 // #region Private Methods
 // -----------------------------------------------------------------------------------------
+
+/**
+ * Binds methods of an object to the object itself, overwriting the existing method. Method names may be
+ * specified as individual arguments or as arrays of method names. If no method names are provided all
+ * enumerable function properties, own and inherited, of object are bound.
+ *
+ * Note: This method does not set the "length" property of bound functions.
+ *
+ * @param object The object to bind and assign the bound methods to.
+ * @param methodNames The object method names to bind, specified as individual method names or arrays of
+ * method names.
+ * @return Returns object.
+ */
+const bindAll = <T>(
+  object: T,
+  ...methodNames: Array<string | string[]>
+): T => _.bindAll(object, ...methodNames);
+
+/**
+ * Creates a function that accepts one or more arguments of func that when called either invokes func returning
+ * its result, if all func arguments have been provided, or returns a function that accepts one or more of the
+ * remaining func arguments, and so on. The arity of func may be specified if func.length is not sufficient.
+ * @param func The function to curry.
+ * @param arity The arity of func.
+ * @return Returns the new curried function.
+ */
+const curry = <T1, R>(
+    func: (t1: T1) => R,
+    arity?: number
+): CurriedFunction1<T1, R> => _.curry(func, arity);
+
+
+/**
+ * Recursively merges own and inherited enumerable properties of source
+ * objects into the destination object, skipping source properties that resolve
+ * to `undefined`. Array and plain object properties are merged recursively.
+ * Other objects and value types are overridden by assignment. Source objects
+ * are applied from left to right. Subsequent sources overwrite property
+ * assignments of previous sources.
+ */
+const merge = <TObject, TSource>(
+  object: TObject,
+  source: TSource
+): TObject & TSource => _.merge(object, source);
+
 
 /**
  * Transforms an enum into an array of its values
@@ -16,8 +67,8 @@ import _ from "lodash";
  * instead of a value of TEnum)
  * @returns {TEnum[]}
  */
-const _enumToArray = <TEnum = any>(enumObject: any): TEnum[] =>
-    _objectToArray(_numericEnumToPojo(enumObject)) as TEnum[];
+const enumToArray = <TEnum = any>(enumObject: any): TEnum[] =>
+    objectToArray(numericEnumToPojo(enumObject)) as TEnum[];
 
 /**
  * Returns a random enum value from its type
@@ -31,11 +82,11 @@ const _enumToArray = <TEnum = any>(enumObject: any): TEnum[] =>
  * @param {TEnum} [excludeElement] A specific enum value to be excluded from the random selection.
  * @returns {TEnum}
  */
-const _getRandomEnum = <TEnum = any>(
+const getRandomEnum = <TEnum = any>(
     enumObject: any,
     excludeElement?: TEnum
 ): TEnum => {
-    let enumValues = _enumToArray(enumObject);
+    let enumValues = enumToArray(enumObject);
 
     if (excludeElement != null) {
         enumValues = enumValues.filter((e: any) => e !== excludeElement);
@@ -44,7 +95,7 @@ const _getRandomEnum = <TEnum = any>(
     return enumValues[Math.floor(Math.random() * enumValues.length)];
 };
 
-const _numericEnumToPojo = (enumObject: any): {} => {
+const numericEnumToPojo = (enumObject: any): {} => {
     let pojo: { [k: string]: any } = {};
 
     for (const key in enumObject) {
@@ -56,7 +107,12 @@ const _numericEnumToPojo = (enumObject: any): {} => {
     return pojo;
 };
 
-const _objectToArray = (object: any): any[] => {
+const memoize = <T extends (...args: any[]) => any>(
+  func: T,
+  resolver?: (...args: any[]) => any
+): T & MemoizedFunction => _.memoize(func, resolver);
+
+const objectToArray = (object: any): any[] => {
     const result: any[] = [];
 
     if (CollectionUtils.isEmpty(object)) {
@@ -73,10 +129,27 @@ const _objectToArray = (object: any): any[] => {
 };
 
 /**
+ * Creates an array of numbers (positive and/or negative) progressing from start up to, but not including, end.
+ * If end is not specified it’s set to start with start then set to 0. If end is less than start a zero-length
+ * range is created unless a negative step is specified.
+ *
+ * @param start The start of the range.
+ * @param end The end of the range.
+ * @param step The value to increment or decrement by.
+ * @return Returns a new range array.
+ */
+const range = (
+    start: number,
+    end?: number,
+    step?: number
+): number[] => _.range(start, end, step);
+
+/**
  * Wrap timeout in a promise so tests can easily block execution for testing time
  * @param milliseconds
+ * @param debug
  */
-const _sleep = (milliseconds: number, debug: boolean = false) => {
+const sleep = (milliseconds: number, debug: boolean = false) => {
     if (debug) {
         console.log("sleep start");
     }
@@ -96,7 +169,7 @@ const _sleep = (milliseconds: number, debug: boolean = false) => {
  * Block execution for specified number of milliseconds, synchronously.
  * @param milliseconds the delay
  */
-const _sleepSync = (milliseconds: number) => {
+const sleepSync = (milliseconds: number) => {
     let now = Date.now();
     const start = now;
     while (now - start < milliseconds) {
@@ -104,13 +177,36 @@ const _sleepSync = (milliseconds: number) => {
     }
 };
 
+
+/**
+ * Creates a throttled function that only invokes func at most once per every wait milliseconds. The throttled
+ * function comes with a cancel method to cancel delayed invocations and a flush method to immediately invoke
+ * them. Provide an options object to indicate that func should be invoked on the leading and/or trailing edge
+ * of the wait timeout. Subsequent calls to the throttled function return the result of the last func call.
+ *
+ * Note: If leading and trailing options are true, func is invoked on the trailing edge of the timeout only if
+ * the the throttled function is invoked more than once during the wait timeout.
+ *
+ * @param func The function to throttle.
+ * @param wait The number of milliseconds to throttle invocations to.
+ * @param options The options object.
+ * @param options.leading Specify invoking on the leading edge of the timeout.
+ * @param options.trailing Specify invoking on the trailing edge of the timeout.
+ * @return Returns the new throttled function.
+ */
+const throttle = <T extends (...args: any[]) => any>(
+  func: T,
+  wait?: number,
+  options?: ThrottleSettings
+): T & Cancelable => _.throttle(func, wait, options);
+
 /**
  * Creates a timer instance that when stopped will supply elapsed time in milliseconds.
  * Useful for benchmarking or providing counters
  * @param name Useful name to identify the timer
  */
-const _timer = (name: string) => {
-    var start = new Date();
+const timer = (name: string) => {
+    const start = new Date();
     return {
         /**
          * Stops the timer and returns elapsed time in milliseconds
@@ -129,6 +225,20 @@ const _timer = (name: string) => {
     };
 };
 
+
+/**
+ * Invokes the iteratee function n times, returning an array of the results of each invocation. The iteratee
+ * is invoked with one argument; (index).
+ *
+ * @param n The number of times to invoke iteratee.
+ * @param iteratee The function invoked per iteration.
+ * @return Returns the array of results.
+ */
+const times = <TResult>(
+  n: number,
+  iteratee: (num: number) => TResult
+): TResult[] => _.times(n, iteratee);
+
 // #endregion Private Methods
 
 // -----------------------------------------------------------------------------------------
@@ -136,20 +246,20 @@ const _timer = (name: string) => {
 // -----------------------------------------------------------------------------------------
 
 export const CoreUtils = {
-    bindAll: _.bindAll,
-    curry: _.curry,
-    enumToArray: _enumToArray,
-    getRandomEnum: _getRandomEnum,
-    memoize: _.memoize,
-    merge: _.merge,
-    numericEnumToPojo: _numericEnumToPojo,
-    objectToArray: _objectToArray,
-    range: _.range,
-    sleep: _sleep,
-    sleepSync: _sleepSync,
-    throttle: _.throttle,
-    timer: _timer,
-    times: _.times,
+    bindAll,
+    curry,
+    enumToArray,
+    getRandomEnum,
+    memoize,
+    merge,
+    numericEnumToPojo,
+    objectToArray,
+    range,
+    sleep,
+    sleepSync,
+    throttle,
+    timer,
+    times,
 };
 
 // #endregion Exports
