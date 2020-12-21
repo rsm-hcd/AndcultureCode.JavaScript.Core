3e9940bc560d98be0ba991e769ed144909dacd40
Brandon Scott <bscott@andculture.com>
2020-12-21 08:42:45 -0500

    Hoist RecordUtils object definition higher in file & attach functions directly to retain JSDoc comments in d.ts files (https://github.com/AndcultureCode/AndcultureCode.JavaScript.Core/issues/51)

--- src/utilities/record-utils.ts
+++ src/utilities/record-utils.ts
@@ -2,43 +2,41 @@ import { Record } from "immutable";
 import { Constructor } from "../types/constructor";
 
 // -----------------------------------------------------------------------------------------
-// #region Functions
+// #region Public Functions
 // -----------------------------------------------------------------------------------------
 
-/**
- * Function to ensure a given object is an instance of a specific Record `T`. If it is not, one will
- * be instantiated with the given constructor.
- *
- * If `maybeRecord` is already an instance of `T`, it returns that value.
- *
- * @param maybeRecord Object or Record to be coalesced into a Record.
- * @param record Type of the Record to be instantiated
- */
-const ensureRecord = <T>(maybeRecord: any, record: Constructor<T>): T =>
-    isRecord(maybeRecord, record) ? maybeRecord : new record(maybeRecord);
+const RecordUtils = {
+    /**
+     * Function to ensure a given object is an instance of a specific Record `T`. If it is not, one will
+     * be instantiated with the given constructor.
+     *
+     * If `maybeRecord` is already an instance of `T`, it returns that value.
+     *
+     * @param maybeRecord Object or Record to be coalesced into a Record.
+     * @param record Type of the Record to be instantiated
+     */
+    ensureRecord<T>(maybeRecord: any, record: Constructor<T>): T {
+        return RecordUtils.isRecord(maybeRecord, record)
+            ? maybeRecord
+            : new record(maybeRecord);
+    },
 
-/**
- * Function to verify a given object is an instance of a specific Record `T`.
- *
- * @param maybeRecord Object or Record to be checked as an instance of `T`
- * @param record Type of the Record to be checked
- */
-const isRecord = <T>(
-    maybeRecord: any,
-    record: Constructor<T>
-): maybeRecord is T =>
-    Record.isRecord(maybeRecord) && maybeRecord instanceof record;
+    /**
+     * Function to verify a given object is an instance of a specific Record `T`.
+     *
+     * @param maybeRecord Object or Record to be checked as an instance of `T`
+     * @param record Type of the Record to be checked
+     */
+    isRecord<T>(maybeRecord: any, record: Constructor<T>): maybeRecord is T {
+        return Record.isRecord(maybeRecord) && maybeRecord instanceof record;
+    },
+};
 
-// #endregion Functions
+// #endregion Public Functions
 
 // -----------------------------------------------------------------------------------------
 // #region Exports
 // -----------------------------------------------------------------------------------------
-
-const RecordUtils = {
-    ensureRecord,
-    isRecord,
-};
 
 export { RecordUtils };
 
