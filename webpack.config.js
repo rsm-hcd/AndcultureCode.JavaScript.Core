1f575647e09236f3ff639635113e99313cacc10f
Brandon Scott <bscott@andculture.com>
2021-06-22 09:02:45 -0400

    0.7.0 Update webpack config and docs script to point to distribution version of tsconfig to exclude test files from doc/bundling processes

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,53 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
 const currentWorkingDirectory = __dirname;
 
-const configPromise = new Promise(function (resolve, reject) {
-
+const configPromise = new Promise(function(resolve, reject) {
     // read the webpack dependency externals config from a separate JSON file
-    fs.readFile('dependency-externals.json', 'utf8', (err, externalsData) => {
+    fs.readFile("dependency-externals.json", "utf8", (err, externalsData) => {
         if (err) {
-            reject(`Unable to read './dependency-externals.json' file.  Reason: ${err}`);
+            reject(
+                `Unable to read './dependency-externals.json' file.  Reason: ${err}`
+            );
         }
 
         let externalsConfig = {};
         try {
             externalsConfig = JSON.parse(externalsData);
-        }
-        catch (e) {
-            reject(`Unable to convert './dependency-externals.json' file data to JSON object.  Reason: ${e}`);
+        } catch (e) {
+            reject(
+                `Unable to convert './dependency-externals.json' file data to JSON object.  Reason: ${e}`
+            );
         }
 
         const webpackConfig = {
-            entry: path.resolve(currentWorkingDirectory, 'src/index.ts'),
-            mode: 'production',
+            entry: path.resolve(currentWorkingDirectory, "src/index.ts"),
+            mode: "production",
             module: {
                 rules: [
                     {
                         test: /\.tsx?$/,
-                        use: 'ts-loader',
+                        use: [
+                            {
+                                loader: "ts-loader",
+                                options: {
+                                    configFile: "tsconfig.dist.json",
+                                },
+                            },
+                        ],
                         exclude: /node_modules/,
                     },
                 ],
             },
             output: {
-                path: path.resolve(__dirname, 'dist-global'),
-                filename: 'index.js',
-                library: 'AndcultureCode',
-                libraryTarget: 'umd',
+                path: path.resolve(__dirname, "dist-global"),
+                filename: "index.js",
+                library: "AndcultureCode",
+                libraryTarget: "umd",
             },
             resolve: {
-                extensions: ['.ts', '.js'],
+                extensions: [".ts", ".js"],
             },
         };
 
