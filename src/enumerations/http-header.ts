d7bde6501234834811c48de0ca7edcb79231ca3c
Brandon Scott <bscott@andculture.com>
2021-06-28 09:13:33 -0400

    Add ContentLength among other common headers

--- src/enumerations/http-header.ts
+++ src/enumerations/http-header.ts
@@ -1,7 +1,13 @@
 enum HttpHeader {
     Accept = "Accept",
+    AcceptEncoding = "Accept-Encoding",
+    AcceptLanguage = "Accept-Language",
     CacheControl = "Cache-Control",
+    ContentLength = "Content-Length",
     ContentType = "Content-Type",
+    Cookie = "Cookie",
+    Date = "Date",
+    UserAgent = "User-Agent",
 }
 
 export { HttpHeader };
